3392c7aee9f24428b7206d694a5b1e0b5c2330b9
33Shivam <sivamrockstar721@gmail.com>
2023-10-23 00:50:02 +0530

    Added Login SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
+    <BrowserRouter>
+      <Theme>
+        <App />
+      </Theme>
+    </BrowserRouter>
   </React.StrictMode>
 );
